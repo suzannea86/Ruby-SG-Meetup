4855dce0738fc0c8600f0cdcdd14479bc8286227
suzannea86 <suzanne.a86@gmail.com>
2013-11-07 11:44:43 +0800

    first commit

---
+++ meetup/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meetup::Application.config.secret_token = '2b8b0e14a34d99b44f4d4e7fea0a22c6ab3b9bef65df780f6c7ced779b9c407c6530bea79a9bc815e7606dff27d167a349946d12aac7d96b2fbb33db688feb9c'
